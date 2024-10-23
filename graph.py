3161ee27dcaca07c835545ac57fb0a3f6d20ebd6
Yaroslav Vitin <36446076+Jarvood11@users.noreply.github.com>
2024-10-23 04:13:57 +0300

    Create graph.py

---
+++ graph.py
@@ -0,0 +1,29 @@
+# Python 3.9.5 (default, May 11 2021, 07:48:02)
+# [GCC 10.3.0] on linux
+# Type "help", "copyright", "credits" or "license" for more information.
+graph = { "a" : ["c"],
+          "b" : ["c", "e"],
+          "c" : ["a", "b", "d", "e"],
+          "d" : ["c"],
+          "e" : ["c", "b"],
+          "f" : []
+        }
+def generate_edges(graph):
+    edges = []
+    for node in graph:
+        for neighbour in graph[node]:
+            edges.append((node, neighbour))
+
+    return edges
+
+print(generate_edges(graph))
+#print("Hello, World!")
+def find_isolated_nodes(graph):
+    """ returns a list of isolated nodes. """
+    isolated = []
+    for node in graph:
+        if not graph[node]:
+            isolated += node
+    return isolated
+
+print(find_isolated_nodes(graph))
